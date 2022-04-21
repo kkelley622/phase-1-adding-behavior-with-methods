29a08ac61312e798abebbfe6cbec23d4c9a5b6eb
Kevin Kelley <kkelley622@gmail.com>
2022-04-21 12:34:02 -0500

    commit tree e53a734d1e54ecc97d9ea24a991c2661b2e97377

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+
+class Cat {
+    constructor(name) {
+        this.name = name;
+        this.speak = function() {
+            return `${this.name} says meow!`;
+        }
+    }
+}
+
+let pepper = new Cat("pepper");
+
+class Dog {
+    constructor(name) {
+        this.name = name;
+        this.speak = function() {
+            return `${this.name} says woof!`;
+        }
+    }
+
+}
+
+let bailey = new Dog("bailey");
+
+class Bird {
+    constructor(name, gender) {
+        this.name = name;
+        this.gender = gender;
+        this.speak = function() {
+            if(this.gender === "male") {
+               return `It's me! ${this.name}, the parrot!`} else{
+                    return `${this.name} says squawk!`;
+                }
+        }
+
+    }
+}
